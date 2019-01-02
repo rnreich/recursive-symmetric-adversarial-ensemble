dfec95bb6a2cc9ec421eebb678e221a71461495a
rnreich <orn.raich@gmail.com>
2019-01-02 12:39:36 +0200

    Entangled optimizers and loss functions

--- wakeup_cycle.py
+++ wakeup_cycle.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from keras.layers import Input, Dense, Dropout
 from keras.models import Sequential, Model
-from keras.optimizers import SGD
+from keras.optimizers import Adadelta, SGD, RMSprop, Adam
 import datetime
 import sys
 import os
@@ -17,28 +17,31 @@ KAGGLE = False
 DATA_FILE = "../input/train.csv" if KAGGLE else "train.csv"
 
 COMP_MEM = 0
-COMP_PROJ1 = 1
-COMP_PROJ2 = 2
-COMP_PRET1 = 3
-COMP_PRET2 = 4
-COMP_ENC = 5
-COMP_DEC = 6
+COMP_PROJ_1 = 1
+COMP_PROJ_2 = 2
+COMP_PRET_1 = 3
+COMP_PRET_2 = 4
+COMP_GATE_IN = 5
+COMP_GATE_OUT = 6
 COMP_OP = 7
 
-HASH_SIZE = 10
+HASH_SIZE = 8
 ALLOW_NON_NUMERIC = True
-DATA_USE_PERCENT = 0.01
+DATA_USE_PERCENT = 0.001
 EPOCHS_PER_FIT = 1
 CYCLES_PER_GLOBAL_EPOCH = 1000
-IN_SIZE = 100
-NUM_SYNAPSES = 10
+IN_SIZE = 84
+NUM_SYNAPSES = 12
 OUT_SIZE = 1
 NUM_FLAGS = 1
 SYN_SIZE = HASH_SIZE * IN_SIZE
 LATENT_DIM = int(SYN_SIZE / NUM_SYNAPSES)
 TARGET_INTELLIGENCE_SIGNAL = 1
 
-targets = np.array([float(TARGET_INTELLIGENCE_SIGNAL)]).reshape((1,NUM_FLAGS))
+print("synapse size: ", SYN_SIZE)
+print("latent size: ", LATENT_DIM)
+
+targets = np.array([float(TARGET_INTELLIGENCE_SIGNAL)]).reshape((1, NUM_FLAGS))
 synapses = []
 
 #------------------------------------------------------------------------------------#
@@ -53,9 +56,9 @@ def softhash(s):
         if len(sx)==1:
             ascii_code = ord(cx)
             if ALLOW_NON_NUMERIC or sx.isnumeric():
-                wf = ascii_code if not sx.isnumeric() else float(sx)/10*256
-                if x>=HASH_SIZE:
-                    a[x%HASH_SIZE] = (a[x%HASH_SIZE] + wf) / 2
+                wf = ascii_code if not sx.isnumeric() else float(sx) / 10 * 256
+                if x >= HASH_SIZE:
+                    a[x % HASH_SIZE] = (a[x % HASH_SIZE] + wf) / 2
                 else:
                     a[x] = wf
 
@@ -67,19 +70,19 @@ def create_synapse():
     pretender1_input = Input(shape=(SYN_SIZE,))
     pretender2_input = Input(shape=(LATENT_DIM,))
 
-    encoder = Sequential()
-    encoder.add(Dense(SYN_SIZE, input_shape=(SYN_SIZE,), activation='tanh'))
-    encoder.add(Dropout(1 / NUM_SYNAPSES))
-    encoder.add(Dense(LATENT_DIM, activation='sigmoid'))
+    gate_in = Sequential()
+    gate_in.add(Dense(SYN_SIZE, input_shape=(SYN_SIZE,), activation='relu'))
+    gate_in.add(Dropout(1 / NUM_SYNAPSES))
+    gate_in.add(Dense(LATENT_DIM, activation='hard_sigmoid'))
 
-    decoder = Sequential()
-    decoder.add(Dense(SYN_SIZE, input_shape=(LATENT_DIM,), activation='tanh'))
-    decoder.add(Dropout(1 / NUM_SYNAPSES))
-    decoder.add(Dense(SYN_SIZE, activation='sigmoid'))
+    gate_out = Sequential()
+    gate_out.add(Dense(SYN_SIZE, input_shape=(LATENT_DIM,), activation='relu'))
+    gate_out.add(Dropout(1 / NUM_SYNAPSES))
+    gate_out.add(Dense(SYN_SIZE, activation='sigmoid'))
 
     task = Sequential()
     task.add(Dense(SYN_SIZE, input_shape=(SYN_SIZE,), activation='relu'))
-    task.add(Dense(OUT_SIZE, activation='sigmoid'))
+    task.add(Dense(OUT_SIZE, activation='hard_sigmoid'))
 
     projector1 = Sequential()
     projector1.add(Dense(SYN_SIZE, input_shape=(LATENT_DIM,), activation='tanh'))
@@ -89,25 +92,25 @@ def create_synapse():
     projector2.add(Dense(SYN_SIZE, input_shape=(SYN_SIZE,), activation='tanh'))
     projector2.add(Dense(NUM_FLAGS, activation='sigmoid'))
 
-    memory = Model(memory_input, decoder(encoder(memory_input)))
-    memory.compile(optimizer=SGD(), loss="mean_squared_error")
+    memory = Model(memory_input, gate_out(gate_in(memory_input)))
+    memory.compile(optimizer=Adadelta(), loss="mean_squared_error")
 
-    operator = Model(memory_input, task(decoder(encoder(memory_input))))
+    operator = Model(memory_input, task(gate_out(gate_in(memory_input))))
     operator.compile(optimizer=SGD(), loss="binary_crossentropy")
 
-    projector1.compile(optimizer=SGD(), loss="mean_squared_error")
+    projector1.compile(optimizer=Adam(), loss="mean_absolute_error")
     projector1.trainable = False
 
-    projector2.compile(optimizer=SGD(), loss="mean_squared_error")
+    projector2.compile(optimizer=Adam(), loss="mean_absolute_error")
     projector2.trainable = False
 
-    pretender1 = Model(pretender1_input, projector1(encoder(pretender1_input)))
-    pretender1.compile(optimizer=SGD(), loss="mean_squared_error")
+    pretender1 = Model(pretender1_input, projector1(gate_in(pretender1_input)))
+    pretender1.compile(optimizer=RMSprop(), loss="binary_crossentropy")
 
-    pretender2 = Model(pretender2_input, projector2(decoder(pretender2_input)))
-    pretender2.compile(optimizer=SGD(), loss="mean_squared_error")
+    pretender2 = Model(pretender2_input, projector2(gate_out(pretender2_input)))
+    pretender2.compile(optimizer=RMSprop(), loss="binary_crossentropy")
 
-    return memory, projector1, projector2, pretender1, pretender2, encoder, decoder, operator
+    return memory, projector1, projector2, pretender1, pretender2, gate_in, gate_out, operator
 
 def load_synapses():
     for i in range(0, NUM_SYNAPSES):
@@ -133,12 +136,12 @@ def save_synapses():
     for i in range(0, NUM_SYNAPSES):
         try:
             synapses[i][COMP_MEM].save_weights("weights/"+str(i)+"_mem")
-            synapses[i][COMP_PROJ1].save_weights("weights/"+str(i)+"_proj1")
-            synapses[i][COMP_PROJ2].save_weights("weights/"+str(i)+"_proj2")
-            synapses[i][COMP_PRET1].save_weights("weights/"+str(i)+"_pret1")
-            synapses[i][COMP_PRET2].save_weights("weights/"+str(i)+"_pret2")
-            synapses[i][COMP_ENC].save_weights("weights/"+str(i)+"_enc")
-            synapses[i][COMP_DEC].save_weights("weights/"+str(i)+"_dec")
+            synapses[i][COMP_PROJ_1].save_weights("weights/"+str(i)+"_proj1")
+            synapses[i][COMP_PROJ_2].save_weights("weights/"+str(i)+"_proj2")
+            synapses[i][COMP_PRET_1].save_weights("weights/"+str(i)+"_pret1")
+            synapses[i][COMP_PRET_2].save_weights("weights/"+str(i)+"_pret2")
+            synapses[i][COMP_GATE_IN].save_weights("weights/"+str(i)+"_enc")
+            synapses[i][COMP_GATE_OUT].save_weights("weights/"+str(i)+"_dec")
             synapses[i][COMP_OP].save_weights("weights/"+str(i)+"_op")
 
             print("saved synapse " + str(i))
@@ -152,7 +155,7 @@ def load_item(row):
     for i in range(0,len(row)):
         v = row[i]
 
-        if i==len(row)-1:
+        if i == len(row) - 1:
             train_y.append(float(v))
         else:
             shash = softhash(str(v))
@@ -160,8 +163,9 @@ def load_item(row):
             for z in range(0, len(shash)):
                 train_x.append(shash[z])
 
-    for i in range(0, IN_SIZE-len(row)+1):
+    for i in range(0, IN_SIZE-len(row) + 1):
         zhash = softhash("0")
+
         for z in range(0, len(zhash)):
             train_x.append(zhash[z])
 
@@ -202,7 +206,7 @@ while True:
                 latents = []
 
                 for i in range(0, NUM_SYNAPSES):
-                    l = synapses[i][COMP_ENC].predict(train_x)
+                    l = synapses[i][COMP_GATE_IN].predict(train_x)
                     latents.append(l)
 
                 biglatent = np.array(latents).reshape(SYN_SIZE)
@@ -216,15 +220,15 @@ while True:
 
                 target_synapse = np.argmin(predictions) if autonomous else cycles%NUM_SYNAPSES
 
-                synapses[target_synapse][COMP_MEM].fit(x=biglatent, y=biglatent, epochs=EPOCHS_PER_FIT*10, batch_size=1, verbose=0)
+                synapses[target_synapse][COMP_MEM].fit(x=biglatent, y=biglatent, epochs=EPOCHS_PER_FIT * 10, batch_size=1, verbose=0)
                 
-                enc_train_x = synapses[target_synapse][COMP_ENC].predict(train_x)
-                dec_train_x = synapses[target_synapse][COMP_DEC].predict(enc_train_x)
+                enc_train_x = synapses[target_synapse][COMP_GATE_IN].predict(train_x)
+                dec_train_x = synapses[target_synapse][COMP_GATE_OUT].predict(enc_train_x)
                 
-                synapses[target_synapse][COMP_PROJ1].fit(x=enc_train_x, y=targets, epochs=EPOCHS_PER_FIT*2, batch_size=1, verbose=0)
-                synapses[target_synapse][COMP_PROJ2].fit(x=dec_train_x, y=targets, epochs=EPOCHS_PER_FIT*2, batch_size=1, verbose=0)
-                synapses[target_synapse][COMP_PRET1].fit(x=train_x, y=targets, epochs=EPOCHS_PER_FIT, batch_size=1, verbose=0)
-                synapses[target_synapse][COMP_PRET2].fit(x=enc_train_x, y=targets, epochs=EPOCHS_PER_FIT, batch_size=1, verbose=0)
+                synapses[target_synapse][COMP_PROJ_1].fit(x=enc_train_x, y=targets, epochs=EPOCHS_PER_FIT * 2, batch_size=1, verbose=0)
+                synapses[target_synapse][COMP_PROJ_2].fit(x=dec_train_x, y=targets, epochs=EPOCHS_PER_FIT * 2, batch_size=1, verbose=0)
+                synapses[target_synapse][COMP_PRET_1].fit(x=train_x, y=targets, epochs=EPOCHS_PER_FIT, batch_size=1, verbose=0)
+                synapses[target_synapse][COMP_PRET_2].fit(x=enc_train_x, y=targets, epochs=EPOCHS_PER_FIT, batch_size=1, verbose=0)
                 
                 f_pred = synapses[target_synapse][COMP_OP].predict(train_x)
                 f_lc_pred = synapses[target_synapse][COMP_OP].predict(biglatent)
@@ -232,8 +236,8 @@ while True:
                 pred = np.around(f_pred[0][0])
                 lc_pred = np.around(f_lc_pred[0][0])
                 diff = lc_success_rate-success_rate
-                if diff<0:
-                    diff=0
+                if diff < 0:
+                    diff = 0
                     
                 wake = False
 
@@ -247,7 +251,7 @@ while True:
                     autonomous = True
                 else:
                     streak = 0
-                    if intelligence_signal==float(1):
+                    if intelligence_signal == float(1):
                         wake = True
                     autonomous = False
 
@@ -255,41 +259,41 @@ while True:
                     successes = successes + 1
 
                 cycles = cycles + 1
-                success_rate =  successes/cycles
-                lc_success_rate =  lc_successes/cycles
+                success_rate =  successes / cycles
+                lc_success_rate =  lc_successes / cycles
 
-                streak_odds = 2**streak
-                beststreak_odds = 2**beststreak
+                streak_odds = 2 ** streak
+                beststreak_odds = 2 ** beststreak
                 
-                ssr = lc_success_rate if lc_success_rate>=0.5 else lc_success_rate/2
-                intelligence_signal = beststreak_odds/cycles*ssr/(1-diff)
-                if intelligence_signal>1:
-                    intelligence_signal=float(1)
+                ssr = lc_success_rate if lc_success_rate >= 0.5 else lc_success_rate / 2
+                intelligence_signal = beststreak_odds / cycles * ssr / (1 - diff) * (1 - success_rate)
+                if intelligence_signal > 1:
+                    intelligence_signal = float(1)
                 
                 flags = np.array([intelligence_signal if autonomous else 0])
                 flags = flags.reshape((1,NUM_FLAGS))
 
-                latent = synapses[target_synapse][COMP_ENC].predict(train_x)
-                dec = synapses[target_synapse][COMP_DEC].predict(latent)
+                latent = synapses[target_synapse][COMP_GATE_IN].predict(train_x)
+                dec = synapses[target_synapse][COMP_GATE_OUT].predict(latent)
 
-                synapses[target_synapse][COMP_PROJ1].fit(x=latent, y=flags, epochs=EPOCHS_PER_FIT*2, batch_size=1, verbose=0)
-                synapses[target_synapse][COMP_PROJ2].fit(x=dec, y=flags, epochs=EPOCHS_PER_FIT*2, batch_size=1, verbose=0)
-                synapses[target_synapse][COMP_OP].fit(x=biglatent, y=train_y, epochs=EPOCHS_PER_FIT*10, batch_size=1, verbose=0)
+                synapses[target_synapse][COMP_PROJ_1].fit(x=latent, y=flags, epochs=EPOCHS_PER_FIT * 2, batch_size=1, verbose=0)
+                synapses[target_synapse][COMP_PROJ_2].fit(x=dec, y=flags, epochs=EPOCHS_PER_FIT * 2, batch_size=1, verbose=0)
+                synapses[target_synapse][COMP_OP].fit(x=biglatent, y=train_y, epochs=EPOCHS_PER_FIT * 10, batch_size=1, verbose=0)
 
-                print("synapse decoder errors: ", predictions)
+                print("data row index: ", data_row_index)
+                print("synapse gate errors: ", predictions)
                 print("epoch id: ", epochs)
                 print("cycle id: ", cycles)
                 print("autonomous: ", autonomous)
                 print("target synapse: ", target_synapse)
                 print("truth: ", truth)
-                print("prediction (data, rounded): ", pred)
                 print("prediction (data): ", f_pred)
                 print("prediction (latent collection): ", f_lc_pred)
                 print("successes (data): ", successes)
                 print("successes (latent collections): ", lc_successes)
                 print("overall proven success rate (data): ", success_rate)
                 print("overall proven success rate (latent collections): ", lc_success_rate)
-                print("lowest decoding error (data): ", predictions[target_synapse])
+                print("lowest gate error: ", predictions[target_synapse])
                 print("streak: ", streak, " (odds = " + str(streak_odds) + ")")
                 print("beststreak: ", beststreak, " (odds = " + str(beststreak_odds) + ")")
                 print("intelligence signal: ", intelligence_signal)
